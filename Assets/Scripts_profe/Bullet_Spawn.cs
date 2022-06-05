2d0f802b58ebea02bfb9988b32921b50b1b0586d
brunopla <102322121+brunopla@users.noreply.github.com>
2022-06-05 16:23:08 -0400

    sound uzi

--- Assets/Scripts_profe/Bullet_Spawn.cs
+++ Assets/Scripts_profe/Bullet_Spawn.cs
@@ -8,11 +8,18 @@ public class Bullet_Spawn : MonoBehaviour
 
     public float speed;
     public float lifeTime;
+    public float rafaga;
+
+    private bool playerIsShooting;
+   
     public GameObject bulletOrigin;
     public GameObject bullet_Uzi;
     public GameObject particle;
     public GameManager gm;
- 
+
+    public AudioClip uzi;
+    AudioSource shoot;
+
 
 
 
@@ -22,27 +29,109 @@ public class Bullet_Spawn : MonoBehaviour
     private void Start()
     {
         gm = FindObjectOfType<GameManager>();
+
+        shoot = GetComponent<AudioSource>();
     }
 
-
     public void Shoot()
-    {
+    {      
         GameObject bullet_Instance = Instantiate(bullet_Uzi, bulletOrigin.transform);
         Instantiate(particle, bulletOrigin.transform);
         bullet_Instance.transform.parent = null;
         gm.ammo--;
 
-        StartCoroutine(BulletDestroy(bullet_Instance, lifeTime));
+        
 
+        StartCoroutine(BulletDestroy(bullet_Instance, lifeTime));
     }
 
+    #region
+    public IEnumerator RafagaBullets()
+    {
+        
+
+        yield return new WaitForSeconds(rafaga /18);
+
+        Shoot();
+
+        yield return new WaitForSeconds(rafaga / 18);
+
+        Shoot();
+
+        yield return new WaitForSeconds(rafaga / 18);
+
+        Shoot();
+
+        yield return new WaitForSeconds(rafaga / 18);
+
+        Shoot();
+
+        yield return new WaitForSeconds(rafaga / 18);
+
+        Shoot();
+
+        yield return new WaitForSeconds(rafaga / 18);
+
+        Shoot();
+
+        yield return new WaitForSeconds(rafaga / 18);
+
+        Shoot();
+
+        yield return new WaitForSeconds(rafaga / 18);
+
+        Shoot();
+
+        yield return new WaitForSeconds(rafaga / 18);
+
+        Shoot();
+
+        yield return new WaitForSeconds(rafaga / 18);
+
+        Shoot();
+
+        yield return new WaitForSeconds(rafaga / 18);
+
+        Shoot();
+
+        yield return new WaitForSeconds(rafaga / 18);
+
+        Shoot();
+
+        yield return new WaitForSeconds(rafaga / 18);
+
+        Shoot();
+
+        yield return new WaitForSeconds(rafaga / 18);
+
+        Shoot();
+
+        yield return new WaitForSeconds(rafaga / 18);
+
+        Shoot();
+
+        yield return new WaitForSeconds(rafaga / 18);
+        playerIsShooting = false;
+    }
+    #endregion
+
+    public void EmpezarRafaga()
+    {
+        if(playerIsShooting == false)
+        {
+            playerIsShooting = true;
+
+            StartCoroutine(RafagaBullets());
+           
+        }
+
+        shoot.PlayOneShot(uzi);
+    }
 
     public IEnumerator BulletDestroy(GameObject g, float t)
     {
         yield return new WaitForSeconds(t);
         Destroy(g);
-        
-
     }
 
    
