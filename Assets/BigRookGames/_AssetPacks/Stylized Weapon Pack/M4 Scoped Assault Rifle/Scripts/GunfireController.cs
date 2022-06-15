d747e9efd1efc3fc692399bc80b71e9ebbf4ba00
brunopla <102322121+brunopla@users.noreply.github.com>
2022-06-14 21:49:43 -0400

    Revert "Revert "Merge branch 'progra' into LVL_design""
    This reverts commit e8135f2f7153ed55e53577ea64483d02f4981afb.

--- Assets/BigRookGames/_AssetPacks/Stylized Weapon Pack/M4 Scoped Assault Rifle/Scripts/GunfireController.cs
+++ Assets/BigRookGames/_AssetPacks/Stylized Weapon Pack/M4 Scoped Assault Rifle/Scripts/GunfireController.cs
@@ -44,20 +44,20 @@ namespace BigRookGames.Weapons
 
         private void Update()
         {
-            // --- If rotate is set to true, rotate the weapon in scene ---
+           
             if (rotate)
             {
                 transform.localEulerAngles = new Vector3(transform.localEulerAngles.x, transform.localEulerAngles.y 
                                                                         + rotationSpeed, transform.localEulerAngles.z);
             }
 
-            // --- Fires the weapon if the delay time period has passed since the last shot ---
+         
             if (autoFire && ((timeLastFired + shotDelay) <= Time.time))
             {
                 FireWeapon();
             }
 
-            // --- Toggle scope based on public variable value ---
+  
             if(scope && lastScopeState != scopeActive)
             {
                 lastScopeState = scopeActive;
@@ -65,26 +65,20 @@ namespace BigRookGames.Weapons
             }
         }
 
-        /// <summary>
-        /// Creates an instance of the muzzle flash.
-        /// Also creates an instance of the audioSource so that multiple shots are not overlapped on the same audio source.
-        /// Insert projectile code in this function.
-        /// </summary>
+ 
         public void FireWeapon()
         {
-            // --- Keep track of when the weapon is being fired ---
+           
             timeLastFired = Time.time;
 
-            // --- Spawn muzzle flash ---
             var flash = Instantiate(muzzlePrefab, muzzlePosition.transform);
 
-            // --- Shoot Projectile Object ---
             if (projectilePrefab != null)
             {
                 GameObject newProjectile = Instantiate(projectilePrefab, muzzlePosition.transform.position, muzzlePosition.transform.rotation, transform);
             }
 
-            // --- Disable any gameobjects, if needed ---
+           
             if (projectileToDisableOnFire != null)
             {
                 projectileToDisableOnFire.SetActive(false);
@@ -94,33 +88,30 @@ namespace BigRookGames.Weapons
             // --- Handle Audio ---
             if (source != null)
             {
-                // --- Sometimes the source is not attached to the weapon for easy instantiation on quick firing weapons like machineguns, 
-                // so that each shot gets its own audio source, but sometimes it's fine to use just 1 source. We don't want to instantiate 
-                // the parent gameobject or the program will get stuck in a loop, so we check to see if the source is a child object ---
                 if(source.transform.IsChildOf(transform))
                 {
                     source.Play();
                 }
                 else
                 {
-                    // --- Instantiate prefab for audio, delete after a few seconds ---
+                   
                     AudioSource newAS = Instantiate(source);
                     if ((newAS = Instantiate(source)) != null && newAS.outputAudioMixerGroup != null && newAS.outputAudioMixerGroup.audioMixer != null)
                     {
-                        // --- Change pitch to give variation to repeated shots ---
+                        
                         newAS.outputAudioMixerGroup.audioMixer.SetFloat("Pitch", Random.Range(audioPitch.x, audioPitch.y));
                         newAS.pitch = Random.Range(audioPitch.x, audioPitch.y);
 
-                        // --- Play the gunshot sound ---
+                       
                         newAS.PlayOneShot(GunShotClip);
 
-                        // --- Remove after a few seconds. Test script only. When using in project I recommend using an object pool ---
+                       
                         Destroy(newAS.gameObject, 4);
                     }
                 }
             }
 
-            // --- Insert custom code here to shoot projectile or hitscan from weapon ---
+          
 
         }
 
